865e4828845eb4a911031cc55a8fc054e4e1e829
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-20 14:20:43 +0300

    change catalog item layout

--- src/components/CatalogItem/CatalogItem.tsx
+++ src/components/CatalogItem/CatalogItem.tsx
@@ -13,8 +13,9 @@ export default function CatalogItem(props: {
 }) {
   return (
     <a href={props.href} target="_blank" rel="noreferrer" className={catalogItem()}>
-      <Avatar src={props.avatarSrc} size={48} />
-      <span className={catalogItem('title')}>{props.title}</span>
+      <div className={catalogItem('wrap')}>
+        <img src={props.avatarSrc} className={catalogItem('avatar')}/>
+      </div>
       <span className={catalogItem('value')}>{props.value}</span>
     </a>
   );
