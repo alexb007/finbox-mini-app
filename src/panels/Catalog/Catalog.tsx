9888ecab55343b95e651b2d145f9934422b3741d
Vitaly Gridasov <vitgridasov@gmail.com>
2021-01-25 14:44:01 +0300

    update link for MoneyMan

--- src/panels/Catalog/Catalog.tsx
+++ src/panels/Catalog/Catalog.tsx
@@ -14,7 +14,7 @@ export default function CatalogPanel(props: IPanelProps): React.ReactElement {
     <Panel id={props.id}>
       <PanelHeader fixed={false}>Каталог</PanelHeader>
       <ContentFix>
-        <CatalogItem href="https://trkleads.ru/click/2eecca95e3566885bed798b4ab4e37d2?aff_sub1=mrelease" avatarSrc="https://i.ibb.co/Xb7VXVZ/moneyman.png" title="МаниМен" value="До 80 000₽" />
+        <CatalogItem href="https://v1.directlead.io/go/VN/29" avatarSrc="https://i.ibb.co/Xb7VXVZ/moneyman.png" title="МаниМен" value="До 80 000₽" />
         <CatalogItem href="https://trkleads.ru/click/60e65901961e9f91b63d994fe4764f10?aff_sub1=mrelease" avatarSrc="https://i.ibb.co/1zgT16c/webbankir-logo.jpg" title="WebBankir" value="До 20 000₽" />
         <CatalogItem href="https://trkleads.ru/click/c26a595d86014acb86f4263ef4638657?aff_sub1=mrelease" avatarSrc="https://i.ibb.co/gjSTfhh/zaymigo-logo.png" title="Zaymigo" value="До 30 000₽" />
         <CatalogItem href="https://trkleads.ru/click/dabbf31c9d54e4d560ae7ce6038a2d9d?aff_sub1=mrelease" avatarSrc="https://i.ibb.co/BrRmhqw/turbozaim.jpg" title="Турбозайм" value="До 50 000₽" />
